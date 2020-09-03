f4b8dce1268790252684b0ceb1189c547100a02e
João Pedrozo <jp.pedrozo@hotmail.com>
2020-09-03 20:13:20 -0300

    adds user id to appointment creation

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -18,9 +18,16 @@ class Appointment {
     @Column()
     provider_id: string;
 
-    @ManyToOne(() => User) // Definindo qual o MODEL
+    @ManyToOne(() => User)
     @JoinColumn({ name: 'provider_id' })
     provider: User;
+
+    @Column()
+    user_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
 
     @Column('timestamp with time zone')
     date: Date;
